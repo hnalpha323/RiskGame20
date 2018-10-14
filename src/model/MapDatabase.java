e5e1501d776214927faf3e44e38f01babd49d57f
hnalpha323 <hnalpha323@gmail.com>
2018-10-14 19:25:06 -0400

    Update src/model/MapDatabase.java

---
+++ src/model/MapDatabase.java
@@ -0,0 +1,49 @@
+
+
+package model;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * @author Muhammad_Hamza_Noor
+ * @version 1.0.0
+ */
+public final class MapDatabase {
+	public static HashMap<String, HashMap<String,Territory>> continents = new HashMap<String, HashMap<String,Territory>>();
+	public static HashMap<String, Integer> continentValues = new HashMap<String, Integer>();		
+
+
+	/**
+	 * @return false if any adjacent territory is not declared in any continent
+	 */
+	public static boolean isValidAdjacency(){
+		Set<String> continentNames = MapDataBase.continents.keySet();
+		for(HashMap<String,Territory> territories : MapDataBase.continents.values()){
+			for(Territory territory:territories.values()){
+			     for(String s : territory.getAdjacentTerritories()){
+			    	 boolean foundTerritory = false;
+			    	 for(String continent: continentNames){
+			    	    if(MapDataBase.continents.get(continent).containsKey(s))
+			    	    	foundTerritory = true;
+			    	 }
+			    	 if(!foundTerritory) return false;
+			     }			    	 
+			}
+			
+		}
+		
+		return true;
+	}
+
+
+	/**
+	 * resets the DataBase
+	 */
+	public static void clear() {
+		continents.clear();
+		continentValues.clear();
+	}
+
+
+}
