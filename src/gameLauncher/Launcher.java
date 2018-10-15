c8c1e83822dfeedb018579dbe2e175b22bbdc614
Meet Patel <meetpatel58@ymail.com>
2018-10-15 17:21:20 -0400

    JavaDoc additions

--- src/gameLauncher/Launcher.java
+++ src/gameLauncher/Launcher.java
@@ -8,14 +8,13 @@ import model.DataReader;
 import model.DataWriter;
 import view.GUIManager;
 
+/** 
+ * Class that contains Main function which will launch the Game
+ * This class initializes controllers,View and connects them.
+ */
+
 public class Launcher {
 
-	/** 
-	 * <li>Step 0: Initialize the controllers</li>
-	 * <li>Step 1: Start the Welcome View</li>
-	 * <li>Step 2: Inject controllers into Views through {@link WindowManager} </li>
-	 * @param args from CMD
-	 */
 	public static void main(String[] args) {
 		 RWMapFileController rwMapFileController = new RWMapFileController();
 		 GameController gameController = new GameController();
