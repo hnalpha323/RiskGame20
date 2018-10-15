5e9bcaaf18af7ebc5924097755976724b2533fc1
hnalpha323 <hnalpha323@gmail.com>
2018-10-14 21:39:49 -0400

    Removed duplication and controller updates

---
+++ src/controller/RWMapFileController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.File;
+
+import model.LoadMap;
+import model.MapDatabase;
+import model.WriteMap;
+
+/**
+ * @author Muhammad_Hamza_Noor
+ * This class controls read and writes on the map
+ */
+public class RWMapFileController{
+
+	/**
+	 * @param file instance of {@link File} points to .map file
+	 */
+	public boolean loadMap(File file) {
+		LoadMap loadMap = new LoadMap(file);
+        return loadMap.load();		
+	}
+
+	
+
+	/**
+	 * @param file instance of {@link File} points to new .map file
+	 */
+	public void writeMap(File file) {
+        WriteMap writeMap = new WriteMap(file);
+        writeMap.write(); 
+	}
+
+
+
+	/**
+	 * Checks the case whether adjacent territories are valid 
+	 * @return true if map satisfies above case
+	 */
+	public boolean validateMap() {
+		return MapDatabase.isValidAdjacency();		
+	}
+
+
+
+	/**
+	 * Clears the previously loaded data from Map
+	 */
+	public void clearData() {
+		MapDatabase.clear();		
+	}	
+
+}
