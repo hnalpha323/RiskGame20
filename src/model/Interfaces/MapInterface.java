57cf953ce538e095e2da62ef0efefd56d355a48a
Meet Patel <meetpatel58@ymail.com>
2018-10-15 20:42:54 -0400

    Added JavaDoc to some files that were missing

--- src/model/Interfaces/MapInterface.java
+++ src/model/Interfaces/MapInterface.java
@@ -3,6 +3,10 @@ package model.Interfaces;
 
 import java.util.ArrayList;
 
+/**
+ * @author Meet_Patel
+ * @version 1.0.0
+ */
 public interface MapInterface {
 
     ArrayList<ContinentInterface> getContinents();
