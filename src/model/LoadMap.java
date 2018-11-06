fa11a83b346b3e271d09ceb2093ff867cb594329
Mian Waleed Ahmad <getwaleedarain@gmail.com>
2018-11-06 16:32:58 -0500

    Load Map

--- src/model/LoadMap.java
+++ src/model/LoadMap.java
@@ -1,79 +1,81 @@
 package model;
 
-import model.Interfaces.ContinentInterface;
-import model.Interfaces.MapInterface;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import java.util.ArrayList;
-
-/**
- * @author Muhammad_Hamza_Noor
- * @version 1.0.0
- * 
- */
-public class Map implements MapInterface {
-
-
-    private ArrayList<ContinentInterface> continents = new ArrayList<>();
-
-    public Map()
-    {
-        this.loadData();
-    }
-
-
-    /**
-     * @return List of all the continents in the map
-     */
-    @Override
-    public ArrayList<ContinentInterface> getContinents() {
-        return this.continents;
-    }
-
-
-    private void loadData(){
-    	this.continents = new ArrayList<>();
-    	for(String continent: MapDatabase.continents.keySet()){
-    		ContinentInterface c = new Continents(continent);
-    		for(Territories Territories: MapDatabase.continents.get(continent).values()){    			
-    			c.addTerritory(Territories);
-    		}
-    		this.continents.add(c);	
-    	}
-    }
-
-    /**
-     * clears all the continent data
-     */
-    public void clearData()
-    {
-        this.continents = new ArrayList<>();
-    }
-
-    /**
-     * Fill map with temp data
-     */
-    public void fakeData()
-    {
-        this.continents = new ArrayList<>();
-
-
-        ContinentInterface c1 = new Continents("Asia");
-        this.continents.add(c1);
-        c1.addTerritory(new Territories("Iran",c1.getName()));
-        c1.addTerritory(new Territories("India",c1.getName()));
-        c1.addTerritory(new Territories("Mexico",c1.getName()));
-        c1.addTerritory(new Territories("Russia",c1.getName()));
-
-        ContinentInterface c2 = new Continents("Africa");
-        this.continents.add(c2);
-        c2.addTerritory(new Territories("Egypt",c2.getName()));
-        c2.addTerritory(new Territories("Kenya",c2.getName()));
-
-        ContinentInterface c3 = new Continents("America");
-        this.continents.add(c3);
-        c3.addTerritory(new Territories("China", c3.getName()));
-        c3.addTerritory(new Territories("Canada", c3.getName()));
-        c3.addTerritory(new Territories("Argentina", c3.getName()));
-
-    }
+public class LoadMap {
+    
+	private File mapFileLocation = null;
+    private Scanner txtScanner = null;
+    
+    
+	/**
+	 * @author WaleedAhmad
+	 * @Version 1.0.0
+	 * 
+	 * @param File Object constructor that contains the map to be loaded
+	 */
+	public LoadMap(File file){
+		mapFileLocation = file;
+	}
+		
+	/**
+	 * @param Gets the map data
+	 * @param Tells what that Data is
+	 * @return boolean depending on if the map is valid or not
+	 */
+	private boolean loadMapToModel(String data,String context){
+		boolean isValidMap = true;
+		switch(context){
+			case "continents":
+				isValidMap = LoadMapUtility.addContinet(data);
+			    break;
+			case "territories":
+				isValidMap = LoadMapUtility.addTerritories(data);
+			default:
+				break;
+		}
+		
+		if(!isValidMap){
+		   	return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * @return Checks if the adjacent territories is valid or not
+	 */
+	public boolean load(){
+		
+	try {
+	txtScanner = new Scanner(new FileInputStream(mapFileLocation));
+	} 
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+		
+		String currentLine = null;
+		String beforeContext = "none";
+		
+		while(txtScanner.hasNextLine()){
+			currentLine = txtScanner.nextLine().trim();
+			if(currentLine.equalsIgnoreCase("[map]")){
+				beforeContext = "map";	
+			}else if(currentLine.equalsIgnoreCase("[continents]")){
+				beforeContext = "continents";
+			} else if(currentLine.equalsIgnoreCase("[territories]")){
+				beforeContext = "territories";
+			}else if(currentLine.length() != 0){
+				if(!loadMapToModel(currentLine, beforeContext))
+					return false;					
+			}else{
+			   	
+			}
+		}
+		
+		return MapDatabase.isValidAdjacency();
+	}
+	
 }
