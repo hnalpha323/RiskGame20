d07ed86c4f728103d5afd855fdd5410ee80e0a6f
meetpatel58 <meetpatel58@ymail.com>
2018-11-07 17:17:50 -0500

    Updated Game Laucher to Support Build 2

--- src/gameLauncher/Launcher.java
+++ src/gameLauncher/Launcher.java
@@ -6,8 +6,13 @@ import controller.ReadController;
 import controller.WriteController;
 import model.DataReader;
 import model.DataWriter;
-
+import model.Model;
+import model.Notifier;
 import view.GUIManager;
+import view.DominationView;
+import view.PhaseView;
+
+
 
 /** 
  * Class that contains Main function which will launch the Game
@@ -19,12 +24,36 @@ import view.GUIManager;
 
 public class Launcher {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		//Creates Controller to controls read and write the .map file
 		 RWMapFileController rwMapFileController = new RWMapFileController();
-		 GameController gameController = new GameController();
+		 
+		 //Creates controller, which is responsible to redirect Read operations
 		 ReadController readController = new ReadController(new DataReader());
+		 
+		 //Creates controller, which is responsible to redirect Write operations
 		 WriteController writeController = new WriteController(new DataWriter());
-		 GUIManager.addRWControllers(rwMapFileController,readController,writeController,gameController);
+		 
+		 //Create Notifier: A model class which sends updates to Views
+		 Model model = new Model();
+		 Notifier notifier = new Notifier();		 
+		 model.setNotifier(notifier);
+		 
+		 //Creates Domination View and make it Observer
+		 DominationView dominationView = new DominationView();
+		 notifier.addObserver(dominationView);
+		 
+		 //Creates phaseView make it Observer
+		 PhaseView phaseView = new PhaseView(dominationView);
+		 notifier.addObserver(phaseView);
+		 
+		 GameController gameController = new GameController();
+		 
+		 //Sends all controllers to view manager, such that views can contact
+		 GUIManager.addControllers(rwMapFileController,readController,writeController,gameController);
+		 GUIManager.setView(phaseView,"phaseview");
+		 
 		 javafx.application.Application.launch(GUIManager.class);
 	}
 }
