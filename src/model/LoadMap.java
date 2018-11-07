aaeecfc03d5bc3b640fb4a025b09bc27e208383d
hnalpha323 <hnalpha323@gmail.com>
2018-11-07 15:35:50 -0500

    Bug Fixes

--- src/model/LoadMap.java
+++ src/model/LoadMap.java
@@ -75,7 +75,7 @@ public class LoadMap {
 			}
 		}
 		
-		return MapDataBase.isValidAdjacency() && MapDataBase.isAnyDiconnectivity();
+		return MapDatabase.isValidAdjacency() && MapDatabase.isAnyDiconnectivity();
 	}
 	
 }
