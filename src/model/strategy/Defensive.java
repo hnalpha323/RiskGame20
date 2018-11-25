c6d7540fa290bb2a4b2735f1e9dc14c1ee5c70cd
hnalpha323 <hnalpha323@gmail.com>
2018-11-25 17:07:51 -0500

    Changes in Strategies

--- src/model/strategy/Defensive.java
+++ src/model/strategy/Defensive.java
@@ -1,22 +1,132 @@
 package model.strategy;
 
+import java.io.Serializable;
+
+import model.AttackPlan;
+import model.Interfaces.PlayerInterface;
 import model.Interfaces.StrategyInterface;
+import model.Interfaces.TerritoryInterface;
+import utility.DiceRNG;
 
-public class Defensive implements StrategyInterface {
+/**
+ * Defensive strategy
+ */
+public class Defensive implements StrategyInterface,Serializable {
+    
+	/**
+	 * Deserialization uses this number to ensure that a loaded class corresponds 
+	 * exactly to a serialized object while saving rhe game state 
+	 */
+	private static final long serialVersionUID = -4744936499083518966L;
 
-
-    public Defensive()
-    {
-
-    }
-
+	/**
+     * how many attacks
+     * @return number of attacks
+     */
     @Override
     public int getAttackAttempts() {
         return 0;
     }
 
+    /**
+     * name of the startegy
+     * @return name
+     */
     @Override
     public String getName() {
         return "Defensive";
+    }
+
+    /**
+     * where to reinforce
+     * @param p player
+     * @return territory
+     */
+    @Override
+    public TerritoryInterface getInforcementTerritory(PlayerInterface p) {
+
+        return p.getWeakestTerritory();
+    }
+
+    /**
+     * set number of reinforcement armies
+     * @param p player
+     * @return number of reinforcement
+     */
+    @Override
+    public int getReinforcementArmies(PlayerInterface p) {
+        return p.getUnusedArmies();
+    }
+
+    /**
+     * Generates an attack plan for a player
+     * @param p player
+     * @return attack plan
+     */
+    @Override
+    public AttackPlan getAttackPlan(PlayerInterface p) {
+        return null;
+    }
+
+    /**
+     * role dice for attacking
+     * @param p player
+     * @return int dice
+     */
+    @Override
+    public int diceToAttack(PlayerInterface p) {
+        return 0;
+    }
+
+    /**
+     * role dice to defend
+     * @param p player
+     * @return dice
+     */
+    @Override
+    public int diceToDefend(PlayerInterface p) {
+        return DiceRNG.getRandomInt(2,1);
+    }
+
+    /**
+     * how many armies should move to new territory
+     * @param p player
+     * @return number of armies
+     */
+    @Override
+    public int getMovingArmiesToNewTerritory(PlayerInterface p) {
+        return DiceRNG.getRandomInt(2,1);
+    }
+
+    /**
+     * where to fortify
+     * @param p player
+     * @return territory
+     */
+    @Override
+    public TerritoryInterface getFortificationFromTerritory(PlayerInterface p) {
+        return p.getStrongestTerritory();
+    }
+
+    /**
+     * where to move fortification armies to
+     * @param p player
+     * @param from origin of fortification
+     * @return destination of fortification
+     */
+    @Override
+    public TerritoryInterface getFortificationToTerritory(PlayerInterface p, TerritoryInterface from) {
+        return p.getWeakestTerritory();
+    }
+
+    /**
+     * number of fortification armies
+     * @param p player
+     * @param from origin of fortification
+     * @return number of armies
+     */
+    @Override
+    public int getFortificationArmies(PlayerInterface p, TerritoryInterface from) {
+        return DiceRNG.getRandomInt(2,1);
     }
 }
--- src/model/strategy/Normal.java
+++ src/model/strategy/Normal.java
@@ -1,21 +1,140 @@
 package model.strategy;
 
+
+import model.AttackPlan;
+import model.Interfaces.PlayerInterface;
 import model.Interfaces.StrategyInterface;
+import model.Interfaces.TerritoryInterface;
+import utility.DiceRNG;
 
-public class Normal implements StrategyInterface {
+import java.io.Serializable;
+import java.util.ArrayList;
 
-    public Normal()
-    {
+/**
+ * Random Strategy
+ */
+public class Normal implements StrategyInterface,Serializable {
 
-    }
+	/**
+	 * Deserialization uses this number to ensure that a loaded class corresponds 
+	 * exactly to a serialized object while saving rhe game state 
+	 */
+	private static final long serialVersionUID = 8419616957685060753L;
 
+	/**
+     * how many attacks
+     * @return number of attacks
+     */
     @Override
     public int getAttackAttempts() {
-        return 1;
+        return utility.DiceRNG.getRandomInt(5,1);
     }
 
+    /**
+     * name of the startegy
+     * @return name
+     */
     @Override
     public String getName() {
         return "Normal";
+    }
+
+    /**
+     * where to reinforce
+     * @param p player
+     * @return territory
+     */
+    @Override
+    public TerritoryInterface getInforcementTerritory(PlayerInterface p) {
+        return p.getRandomTerritory();
+    }
+
+    /**
+     * set number of reinforcement armies
+     * @param p player
+     * @return number of reinforcement
+     */
+    @Override
+    public int getReinforcementArmies(PlayerInterface p) {
+        return utility.DiceRNG.getRandomInt(p.getUnusedArmies(),1);
+    }
+
+    /**
+     * where to fortify
+     * @param p player
+     * @return territory
+     */
+    @Override
+    public TerritoryInterface getFortificationFromTerritory(PlayerInterface p) {
+        return p.getRandomTerritory();
+    }
+
+    /**
+     * where to move fortification armies to
+     * @param p player
+     * @param from origin of fortification
+     * @return destination of fortification
+     */
+    @Override
+    public TerritoryInterface getFortificationToTerritory(PlayerInterface p, TerritoryInterface from) {
+        TerritoryInterface to;
+
+        ArrayList<TerritoryInterface> neighbours = from.getAdjacentNeighbours();
+        if(neighbours.size()>0)
+            to = neighbours.get(0);
+        else
+            to = p.getRandomTerritory();
+
+        return to;
+    }
+
+    /**
+     * number of fortification armes
+     * @param p player
+     * @param from origin of fortification
+     * @return number of armies
+     */@Override
+    public int getFortificationArmies(PlayerInterface p, TerritoryInterface from) {
+        return DiceRNG.getRandomInt(from.getArmies(),1);
+    }
+
+    /**
+     * Generates an attack plan for a player
+     * @param p player
+     * @return attack plan
+     */
+    @Override
+    public AttackPlan getAttackPlan(PlayerInterface p) {
+        return p.getTerritoryToAttack();
+    }
+
+    /**
+     * role dice for attacking
+     * @param p player
+     * @return int dice
+     */
+    @Override
+    public int diceToAttack(PlayerInterface p) {
+        return DiceRNG.getRandomInt(3,1);
+    }
+
+    /**
+     * role dice to defend
+     * @param p player
+     * @return dice
+     */
+    @Override
+    public int diceToDefend(PlayerInterface p) {
+        return DiceRNG.getRandomInt(2,1);
+    }
+
+    /**
+     * how many armies should move to new territory
+     * @param p player
+     * @return number of armies
+     */
+    @Override
+    public int getMovingArmiesToNewTerritory(PlayerInterface p) {
+        return 1;
     }
 }
