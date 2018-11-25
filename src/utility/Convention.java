30b86e672fad9d113141f4fa3a97125859630b4c
Mostakim <shah.md.mostakim@gmail.com>
2018-11-24 23:08:34 -0500

    coding convention

---
+++ src/utility/Convention.java
@@ -0,0 +1,20 @@
+package utility;
+/**
+ * Coding convention used in this project 
+ * (will work on later)
+ * @author Shah Mohammad Mostakim
+ * @version 3.0
+ */
+public class Convention {
+	public Convention() {}
+	private boolean m_sampleBooleanVariable = true;
+
+    public boolean getSampleBooleanVariable()
+    {
+        return this.m_sampleBooleanVariable;
+    }
+
+    public void setSampleBooleanVariable(boolean whatever){
+        this.m_sampleBooleanVariable = whatever;
+    }
+}
