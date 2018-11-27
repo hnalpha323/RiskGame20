a347c9cf6e3f523be9d538c894b03ae6e860d7a0
hnalpha323 <hnalpha323@gmail.com>
2018-11-27 16:37:40 -0500

    TestSuite

---
+++ test/suite/TestSuite.java
@@ -0,0 +1,35 @@
+package suite;
+
+import org.junit.runner.RunWith;
+import model.*;
+import mapFiles.*;
+import controller.*;
+import model.strategy.*;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ReadControllerTest.class,
+	WriteControllerTest.class,
+	ReaderTest.class,
+	ValidMapTest.class,
+	StrategyTest.class,
+	ArmiesUseTest.class,
+	AttackTest.class,
+	ControlPlayerTest.class,
+	CardExchangeTest.class,
+	InitialArmiesTest.class,
+	InvalidMoveTest.class,
+	MaxPlayersTest.class,
+	MinPlayerTest.class,
+	MinReinforceTest.class,
+	NoMapTest.class,
+	SaveMethodTest.class,
+	
+})
+public class TestSuite {
+	
+
+
+}
