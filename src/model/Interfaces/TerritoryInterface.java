2588ed75862a5fb56963421f1deae1d06a6dd44b
hnalpha323 <hnalpha323@gmail.com>
2018-11-27 18:55:19 -0500

    Update TerritoryInterface

--- src/model/Interfaces/TerritoryInterface.java
+++ src/model/Interfaces/TerritoryInterface.java
@@ -4,23 +4,22 @@ import utility.Results;
 
 import java.util.ArrayList;
 
-import model.Interfaces.TerritoryInterface;
 
 /**
- * @author Meet_Patel
- * @version 1.0.0
+ * Business functions for Territory
  */
 public interface TerritoryInterface {
 
-	void setOwner(PlayerInterface player);
+    void setOwner(PlayerInterface player);
     PlayerInterface getOwner();
     String getName();
+    ContinentInterface getContinent();
+    void setContinent(ContinentInterface c);
 
     int getArmies();
     void placeArmies(int count);
     Results removeArmies(int count);
     Results killArmies(int count);
-
     boolean hasAdjacencyWith(TerritoryInterface t);
     ArrayList<TerritoryInterface> getAdjacentTerritoryObjects();
     ArrayList<TerritoryInterface> getAdjacentNeighbours();
