4b9db5238ce75bb373cd02e2f942eecf839205d9
meetpatel58 <meetpatel58@ymail.com>
2018-11-27 19:11:07 -0500

    Errors fixes

--- src/model/Interfaces/ContinentInterface.java
+++ src/model/Interfaces/ContinentInterface.java
@@ -2,6 +2,9 @@ package model.Interfaces;
 
 import java.util.ArrayList;
 
+import model.Interfaces.PlayerInterface;
+import model.Interfaces.TerritoryInterface;
+
 /**
  * @author Meet_Patel
  * @version 1.0.0
@@ -9,14 +12,11 @@ import java.util.ArrayList;
 public interface ContinentInterface {
 	
 	ArrayList<TerritoryInterface> getTerritories();
-	String getName();
-	
-	int getContinentValue();
-	void setContinentValue(int value);
-	
     void addTerritory(TerritoryInterface t);
     void removeTerritory(TerritoryInterface t);
-    
+    String getName();
+    int getContinentValue();
+    void setContinentValue(int value);
     boolean controlByPlayer(PlayerInterface p);
 
 }
