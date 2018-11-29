6d900cf021ad119145319a8b704fbc402b614eee
meetpatel58 <meetpatel58@ymail.com>
2018-11-28 19:04:31 -0500

    Code now follows Coading standards

--- src/controller/LoggerController.java
+++ src/controller/LoggerController.java
@@ -3,26 +3,29 @@ package controller;
 import utility.MessageEnum;
 
 /**
- * TO send notify View and send messages to the view
+ * To send notify View and send messages to the view
  * @author Muhammad_Hamza_Noor
  * @version 1.0.0
  */
-public class LoggerController {
+public class LoggerController 
+{
 
 
 	/**
 	 * @param Message Enumerator
 	 * @param Content of message
 	 */
-	public static void log(MessageEnum tag, String message){
+	public static void log(MessageEnum tag, String message)
+	{
 		view.Message.log(tag, message);
 	}
 
 	/**
-	 * TO log the message to View when called
+	 * To log the message to View when called
 	 * @param Content of message
 	 */
-	public static void log(String message){
+	public static void log(String message)
+	{
 		view.Message.log(message);
 	}
 
--- src/controller/RWMapFileController.java
+++ src/controller/RWMapFileController.java
@@ -10,53 +10,48 @@ import model.ValidMap;
 /**
  * @author Muhammad_Hamza_Noor
  * @author Meet Patel
- * <p>
- * To Load and Save a .map file into {@link MapDataBase}
+ * To Load and Save a .map file into MapDataBase
  * A view should call this class methods to load, save and clear a .map file
- * </p> 
  */
-public class RWMapFileController{
+public class RWMapFileController
+{
 
 	/**
-	 * @param file which has path to .map file
-	 * @return true is .map file is valid else false
-	 * <p>
-	 * If a map is disconnected it return false for more see
-	 * <a href="http://www.windowsgames.co.uk/conquest_create.html">here</a>
-	 * </p>
+	 * @param File which has path to .map file
+	 * @return True if .map file is valid else false
+	 * If a map is disconnected it return false
 	 */
-	public boolean loadMap(File file) {
+	public boolean loadMap(File file) 
+	{
 		LoadMap loadMap = new LoadMap(file);
         boolean isValid = loadMap.load();
 		return isValid;		
 	}
 
-	
-
 	/**
 	 * This method writes the edited map to the location passed
 	 * @param file instance of {@link File} points to save destination
 	 */
-	public void writeMap(File file) {
+	public void writeMap(File file) 
+	{
         MapWriter writeMap = new MapWriter(file);
         writeMap.write(); 
 	}
 
-
 	/**
 	 * Clears the previously loaded data from Map
 	 */
-	public void clearData() {
+	public void clearData() 
+	{
 		MapDatabase.clear();		
 	}
 
-	
-	
 	/**
 	 * Checks the case whether adjacent territories are valid 
-	 * @return true if map satisfies above case
+	 * @return True if map satisfies above case
 	 */
-	public boolean validateMap() {
+	public boolean validateMap() 
+	{
 		return ValidMap.isValidAdjacency();		
 	}
 
