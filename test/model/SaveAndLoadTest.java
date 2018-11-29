b1fd87fdcfa047610666688b54c19662977b1fc4
ShahMostakim <shah.md.mostakim@gmail.com>
2018-11-29 14:41:19 -0500

    check game name after load

--- test/model/SaveAndLoadTest.java
+++ test/model/SaveAndLoadTest.java
@@ -41,5 +41,33 @@ public class SaveAndLoadTest {
 		assertEquals("Initialize", expectedPhase);
 		
 	}
+	
+	/**
+	 * Saves Game with a name and checks if its same after loading again
+	 */
+	@Test
+	public void testGameNameAfterLoad() throws IOException, PlayerException {
+		
+		Map map = new Map();
+		map.clearData();
+		map.fakeData();
+		
+		// create a gamedriver
+		// total number of turns is 300
+		GameDriver GD = new GameDriver(map, 3, "b,a,r", 300);
+		GD.start(false);
+		GD.setName("Conquest");
+		
+		// save game driver
+		SaveMethod sm = new SaveMethod();
+		sm.saveState(GD);
+		
+		// load gamedriver back from file 
+		GameDriver G = sm.getPreviousState();
+		String expectedName = G.getName();
+		// after loading GameDriver again game phase is same like before
+		assertEquals("Conquest", expectedName);
+		
+	}
 
 }
